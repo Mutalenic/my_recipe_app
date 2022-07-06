e7896ad7e8ce25d5db55fc21204477ade91bfab0
Nicholas Mutale <nicomutale@gmail.com>
2022-07-06 16:57:06 +0200

    Add form to new create delete button update food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,26 +3,31 @@ class FoodsController < ApplicationController
         @foods = Food.all
     end
 
-    def show
-        @food = Food.find(params[:id])
-    end
-
-    def new
-        @food = Food.new
-    end
+    def new; end
 
     def create
         @food = Food.new(food_params)
+        @food.user_id = current_user.id
+
         if @food.save
-            redirect_to foods_path
+            redirect_to foods_path, notice: 'Food was successfully created.'
         else
-            render :new
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @food = Food.find(params[:id])
+        if @food.destroy
+            redirect_to foods_path, notice: 'Food was successfully deleted.'
+        else
+            render :new, notice: 'Food was not deleted.'
         end
     end
 
     private
 
     def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+        params.permit(:name, :measurement_unit, :price, :quantity, :user_id)
     end
 end
