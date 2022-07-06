e44a33b91978d7f3bdca92948da3f5e5275f4940
Nicholas Mutale <nicomutale@gmail.com>
2022-07-06 08:06:34 +0200

    Fixed require rails helper

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -7,11 +7,4 @@ RSpec.describe 'users/new', type: :view do
                   ))
   end
 
-  it 'renders new user form' do
-    render
-
-    assert_select 'form[action=?][method=?]', users_path, 'post' do
-      assert_select 'input[name=?]', 'user[name]'
-    end
-  end
 end
