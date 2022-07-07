03773d9ba24686e2dafcd1a3c574a29e484adadf
Nicholas Mutale <nicomutale@gmail.com>
2022-07-07 17:43:40 +0200

    Revert "Public Recipe list"

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes
   resources :foods, only: [:index, :show, :new, :create, :destroy]
-  resources :food_recipes resources :recipes do
-    resources :recipe_foods, only:[:new,:create,:destroy]
- end
- get "/public_recipes" , to: "recipes#public_recipes"# Defines the root path route ("/")
+  resources :food_recipes
+  # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show] do
   end
-  root "recipes#public_recipes"
+  root "users#index"
 end
