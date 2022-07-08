7f01530b8cb4a1e3eb707150a090e5eaab3d5f41
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-07-08 10:07:00 +0200

    Fix the linters errors using rubocop

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     cannot :read, :all
-    can :create, :all
-    can :destroy, :all, user_id: user_id 
     return unless user.present?
+
+    can :create, :all
+    can :destroy, :all, user_id:
   end
 end
